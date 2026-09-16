43b75c79549ea19d1df76ec9078a685a31de7dd2
William <wla230@sfu.ca>
2026-09-16 12:06:21 -0700

    initial code

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -3,6 +3,26 @@
 
 int main(){
 
+    char *buff = NULL;
+    size_t size = 0;
+    ssize_t num_char = 0;
+
+    while(){
+    printf("Please enter some text:");
+    ssize_t user_input = getline(&buff, &size, stdin);
+    if(num_char == -1){
+        perror("getline failed");
+        exit(EXIT_FAILURE);
+        free(buff);
+    }
+    for (j = 1, &buff = argv[1]; ; j++, &buff = NULL) {
+        token = strtok_r(&buff, argv[2], &saveptr1);
+        if (token == NULL)
+            break;
+        printf("%d: %s\n", j, token);
+        ...
+    }
+    }
 
 
     return 0;
